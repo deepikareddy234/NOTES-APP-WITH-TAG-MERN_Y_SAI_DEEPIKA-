857b3edd979cbcfbcca42f888c51967df8475e25
deepikareddy234 <deepikayerlam7@gmail.com>
2025-07-15 23:15:07 +0530

    updated 10

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,11 +1,10 @@
 import express from "express";
 import { signin, signout, signup } from "../controller/auth.controller.js";
-import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
-router.get("/signout", verifyToken, signout);
+router.get("/signout", signout); // ✅ removed verifyToken
 
 export default router;
